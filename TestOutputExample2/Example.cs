5fea2d59d6b693a08442a277247576de00e916b4
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2021-11-25 13:23:00 +0100

    Update Example.cs

--- TestOutputExample2/Example.cs
+++ TestOutputExample2/Example.cs
@@ -9,18 +9,63 @@ namespace MyFirstUnitTests
         [Fact]
         public void PassingTest()
         {
+            System.Threading.Thread.Sleep(25000);
             Assert.Equal(4, Add(2, 2));
+            
         }
 
         [Fact]
         public void FailingTest()
         {
+            System.Threading.Thread.Sleep(40000);
             Assert.Equal(5, Add(2, 2));
+            
         }
 
         int Add(int x, int y)
         {
+            System.Threading.Thread.Sleep(24000);
             return x + y;
         }
+        
+         [Fact]
+        
+        public void FailingTest2()
+        {
+            System.Threading.Thread.Sleep(40000);
+            Assert.Equal(5, Add(2, 2));
+            
+        }
+
+        int Add(int x, int y)
+        {
+           
+            return x + y;
+        }
+         public void FailingTest3()
+        {
+            System.Threading.Thread.Sleep(50000);
+            Assert.Equal(5, Add(2, 2));
+            
+        }
+
+        int Add(int x, int y)
+        {
+           
+            return x + y;
+        }
+        public void FailingTest4()
+        {
+            System.Threading.Thread.Sleep(10000);
+            Assert.Equal(5, Add(2, 2));
+            
+        }
+
+        int Add(int x, int y)
+        {
+           
+            return x + y;
+        }
+    }
     }
 }
